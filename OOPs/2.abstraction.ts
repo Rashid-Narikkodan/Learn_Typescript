3ddf61f957758f44b31d7fc25b0570acf1de8c9b
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2025-12-20 23:17:07 +0530

    Abstraction using abstract class in OOPs

--- OOPs/2.abstraction.ts
+++ OOPs/2.abstraction.ts
@@ -20,9 +20,25 @@ class OrderSerivce{
 const order1 = new OrderSerivce(new Razorpay)
 order1.placeOrder({product:'laptop',price:34})
 
-/* -------------------------------------------------------------------------------------- */
+/*------------------------------------------------------------------------------------------------------*/
 
 // 2.abstraction using abstract class
+
+abstract class PaymentGateWay{
+    constructor(protected apiKey:string){}   // can use constructor in abstract class
+    abstract pay(moeny:number):string        // abstract methods in abstract class
+    log(money:number):void{                  //we can write normal methods also in abstract class
+        console.log('payment happened')
+    }
+}
+
+class RazorPay1 extends PaymentGateWay{
+    pay(){
+        return ''
+    }
+    log(money:number){}// we can also use a method thats exists in that abstract class 
+    // (not neccessary to overrride log(), but pay() is neccessary bcz tats abstract method)
+}
 
 
 
